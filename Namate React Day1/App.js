c5568731e6aad79b3a175d6e2cf40b119a8cf0b8
Rahul Chouhan <78010526+Rahulchouhan100@users.noreply.github.com>
2022-12-25 12:56:01 +0530

    Add files via upload

---
+++ Namate React Day1/App.js
@@ -0,0 +1,13 @@
+// Day 01 - Namaste React 
+// let root = document.getElementById("root")
+// let h1 = document.createElement("h1");
+// let text = document.createTextNode("This is heading Tag")
+// h1.appendChild(text)
+// root.appendChild(h1);
+
+// // React ----------- 
+
+// const heading = React.createElement("h1",{},"this is heading");
+// const root = ReactDom.CreateRoot(document.getElementById("root"));
+
+// root.render(heading);
